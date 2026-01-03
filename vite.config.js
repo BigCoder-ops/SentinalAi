3b349570df1a32fc1588065ca684e93fb352fb7d
Mohamed Mahzoul <111699844+BigCoder-ops@users.noreply.github.com>
2026-01-03 13:03:01 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-
-
-// Replace 'repo-name' with the exact name of your GitHub repository
+// Replace 'sentinel-ai' with the EXACT name of your GitHub repository
 export default defineConfig({
   plugins: [react()],
-  base: '/repo-name/', 
+  base: '/sentinel-ai/', 
 })
